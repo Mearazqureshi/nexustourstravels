47a71cb0c8a3d4db06469b952a42738dd0acf1fb
Mearaz Qureshi <mearazqureshi@gmail.com>
2017-08-17 19:27:44 +0530

    bug fixing

--- resources/views/package/index.blade.php
+++ resources/views/package/index.blade.php
@@ -49,16 +49,16 @@
 
         @else
 
-            <div class="col-md-2 sortby-box wow flipInY animated " style="width:21.666667%" style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;" data-wow-duration="1000ms" data-wow-delay="300ms">
-                <div class="col-md-12">
+            <div class="col-md-2 sortby-box wow flipInY animated padding0" style="width:21.666667%" style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;" data-wow-duration="1000ms" data-wow-delay="300ms">
+                <div class="col-md-12 padding0">
                     <h2>Price</h2>
                 </div>
                 
-                <div class="col-md-12">
+                <div class="col-md-12 padding10">
                     <input class="price-radio" data-value="ASC" type="radio" name="price" value="low"> Low to High
                 </div>
 
-                <div class="col-md-12">
+                <div class="col-md-12 padding5">
                     <input class="price-radio" data-value="DESC" type="radio" name="price" value="high"> High to Low
                 </div>
 
@@ -66,7 +66,7 @@
 
             @foreach($packages as $package)
                 <div id="packages_list">
-                    <div class="col-md-9 package-item team-member wow flipInY animated" style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;" data-wow-duration="1000ms" data-wow-delay="300ms">
+                    <div class="col-md-9 margin190 package-item team-member wow flipInY animated" style="visibility: visible; animation-duration: 1000ms; animation-delay: 300ms; animation-name: flipInY;" data-wow-duration="1000ms" data-wow-delay="300ms">
                     
                         <div class="col-md-4 no-padding">
                             <img src="{{ url('Packages',$package->image) }}" class="package-list-image">
